03a1f6e77b5acc10d114c9487972661e2f634509
coursemdetw <course@mde.tw>
2016-01-06 16:52:47 +0800

    update data

--- local_publishconf.py
+++ local_publishconf.py
@@ -31,7 +31,7 @@ DELETE_OUTPUT_DIRECTORY = True
 
 # Following items are often useful when publishing
 
-DISQUS_SITENAME = "kmolab"
+#DISQUS_SITENAME = "kmolab"
 #GOOGLE_ANALYTICS = ""
 
 # 設定網誌以 md 檔案建立的 file system date 為準, 無需自行設定
