35010825e3156063bc0534cb281f7ebbb0c588e5
coursemdetw <course@mde.tw>
2016-01-06 12:21:20 +0800

    remove author list to see if disqus function coming back2

--- local_publishconf.py
+++ local_publishconf.py
@@ -31,7 +31,7 @@ DELETE_OUTPUT_DIRECTORY = True
 
 # Following items are often useful when publishing
 
-#DISQUS_SITENAME = "kmolab"
+DISQUS_SITENAME = "kmolab"
 #GOOGLE_ANALYTICS = ""
 
 # 設定網誌以 md 檔案建立的 file system date 為準, 無需自行設定
@@ -45,3 +45,4 @@ ARTICLE_URL = 'posts/{date:%Y}/{date:%m}/{date:%d}/{slug}/index.html'
 ARTICLE_SAVE_AS = 'posts/{date:%Y}/{date:%m}/{date:%d}/{slug}/index.html'
 PAGE_URL = 'pages/{slug}/'
 PAGE_SAVE_AS = 'pages/{slug}/index.html'
+SHOW_ARTICLE_AUTHOR = True
--- publishconf.py
+++ publishconf.py
@@ -45,3 +45,4 @@ ARTICLE_URL = 'posts/{date:%Y}/{date:%m}/{date:%d}/{slug}/'
 ARTICLE_SAVE_AS = 'posts/{date:%Y}/{date:%m}/{date:%d}/{slug}/index.html'
 PAGE_URL = 'pages/{slug}/'
 PAGE_SAVE_AS = 'pages/{slug}/index.html'
+SHOW_ARTICLE_AUTHOR = True
